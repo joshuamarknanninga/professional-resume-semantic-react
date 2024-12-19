28a5155bb73dfba12fb77e951f00f9cc10fc0a89
Joshua Nanninga <jmarknanninga@gmail.com>
2024-12-19 09:54:50 -0600

    commit with structure and components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles/globals.css'; // Import global styles
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+
+// Check if the container exists
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
